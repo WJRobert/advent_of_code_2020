b40c17dff16050ca4d66bf10234e0d81f65f9053
Wayne Robert <wayne.j.robert@gmail.com>
2020-12-05 07:59:19 +0200

    Cleaned up day 5

--- AdventOfCode/src/main/java/za/co/wayne/j/robert/advent/of/code/puzzle/day/five/PuzzleSolver.java
+++ AdventOfCode/src/main/java/za/co/wayne/j/robert/advent/of/code/puzzle/day/five/PuzzleSolver.java
@@ -3,12 +3,14 @@ package za.co.wayne.j.robert.advent.of.code.puzzle.day.five;
 import za.co.wayne.j.robert.advent.of.code.util.FileReader;
 
 import java.util.List;
-import java.util.Map;
 
 public class PuzzleSolver {
 
     private final static int ROW_MAX = 127;
     private final static int COL_MAX = 7;
+    private final static int ROW_INDICATOR_LENGTH = 7;
+    private final static char ROW_BOTTOM_HALF_INDICATOR = 'F';
+    private final static char COL_BOTTOM_HALF_INDICATOR = 'L';
 
     private FileReader fileReader;
 
@@ -17,12 +19,10 @@ public class PuzzleSolver {
     }
 
     public int solvePuzzleOne(String inputFilePath) {
-        List<String> inputs = fileReader.readInput(inputFilePath);
+        List<String> boardingPassList = fileReader.readInput(inputFilePath);
         int highestSeatId = 0;
-        for (String input : inputs) {
-            int rowId = findNextRemainder(input.substring(0,7), 0 , ROW_MAX, 'F');
-            int colId = findNextRemainder(input.substring(7), 0 , COL_MAX, 'L');
-            int seatId = (rowId*8)+colId;
+        for (String boardingPass : boardingPassList) {
+            int seatId = calculateSeatId(boardingPass);
             if (seatId > highestSeatId) {
                 highestSeatId = seatId;
             }
@@ -30,39 +30,42 @@ public class PuzzleSolver {
         return highestSeatId;
     }
 
-    private int findNextRemainder(String remainder, int min, int max, char bottomHalf) {
-        char nextRow = remainder.charAt(0);
-        int remainderRowLength = remainder.length();
-        int diffHalf = (max+1 - min)/2;
-        if (nextRow == bottomHalf) {
+    private int calculateSeatId(String boardingPass) {
+        int rowId = findBoardingPassIndex(boardingPass.substring(0, ROW_INDICATOR_LENGTH), 0, ROW_MAX, ROW_BOTTOM_HALF_INDICATOR);
+        int colId = findBoardingPassIndex(boardingPass.substring(ROW_INDICATOR_LENGTH), 0, COL_MAX, COL_BOTTOM_HALF_INDICATOR);
+        return (rowId * 8) + colId;
+    }
+
+    private int findBoardingPassIndex(String boardingPass, int min, int max, char bottomHalf) {
+        char nextBoardingPassIndex = boardingPass.charAt(0);
+        int boardingPassLength = boardingPass.length();
+        int diffHalf = (max + 1 - min) / 2;
+        if (nextBoardingPassIndex == bottomHalf) {
             //Front half
-            if (remainderRowLength == 1) {
+            if (boardingPassLength == 1) {
                 return min;
             } else {
-                return findNextRemainder(remainder.substring(1), min, min+diffHalf-1, bottomHalf);
+                return findBoardingPassIndex(boardingPass.substring(1), min, min + diffHalf - 1, bottomHalf);
             }
         } else {
             //Back half
-            if (remainderRowLength == 1) {
+            if (boardingPassLength == 1) {
                 return max;
             } else {
-                return findNextRemainder(remainder.substring(1), max+1-diffHalf, max, bottomHalf);
+                return findBoardingPassIndex(boardingPass.substring(1), max + 1 - diffHalf, max, bottomHalf);
             }
         }
     }
 
     public int solvePuzzleTwo(int highestSeat, String inputFilePath) {
-        List<String> inputs = fileReader.readInput(inputFilePath);
+        List<String> boardingPassList = fileReader.readInput(inputFilePath);
         int[] seats = new int[highestSeat];
-        for (String input : inputs) {
-            int rowId = findNextRemainder(input.substring(0,7), 0 , ROW_MAX, 'F');
-            int colId = findNextRemainder(input.substring(7), 0 , COL_MAX, 'L');
-            int seatId = (rowId*8)+colId;
-            seats[seatId-1] = 1;
+        for (String boardingPass : boardingPassList) {
+            seats[calculateSeatId(boardingPass) - 1] = 1;
         }
-        for (int i=1; i<highestSeat-2; i++) {
-            if (seats[i] != 1 && seats[i-1] == 1 && seats[i+1] == 1) {
-                return i+1;
+        for (int i = 1; i < highestSeat - 2; i++) {
+            if (seats[i] != 1 && seats[i - 1] == 1 && seats[i + 1] == 1) {
+                return i + 1;
             }
         }
         return 0;
